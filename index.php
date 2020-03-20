09d0192c738c40fa459940cd0c23d1114210d327
muhammadnorseputra <muhammadnorseputra@gmail.com>
2020-03-20 22:25:03 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--
+    Document Title
+    =============================================
+    -->
+    <title>BKPPD | Badan Kepegawaian Pendidikan dan Pelatihan Daerah Kab. Balangan</title>
+    <!--
+    Favicons
+    =============================================
+    -->
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/logo.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="assets/images/logo.png">
+    <meta name="theme-color" content="#ffffff">
+    <!--
+    Stylesheets
+    =============================================
+    
+    -->
+    <!-- Default stylesheets-->
+    <link href="assets/lib/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Template specific stylesheets-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Volkhov:400i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
+    <link href="assets/lib/animate.css/animate.css" rel="stylesheet">
+    <link href="assets/lib/components-font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/lib/et-line-font/et-line-font.css" rel="stylesheet">
+    <link href="assets/lib/flexslider/flexslider.css" rel="stylesheet">
+    <link href="assets/lib/magnific-popup/dist/magnific-popup.css" rel="stylesheet">
+    <link href="assets/lib/simple-text-rotator/simpletextrotator.css" rel="stylesheet">
+    <!-- Main stylesheet and color file-->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link id="color-scheme" href="assets/css/colors/default.css" rel="stylesheet">
+  </head>
+  <body data-spy="scroll" data-target=".onpage-navigation" data-offset="60">
+    <main>
+      <div class="page-loader">
+        <div class="loader">Loading...</div>
+      </div>
+      <section class="home-section bg-dark-30" id="home">
+
+        <div class="video-player"></div>
+        <div class="video-controls-box">
+          <div class="container">
+            <div class="video-controls"><a class="fa fa-volume-up" id="video-volume" href="#">&nbsp;</a>
+            <a class="fa fa-pause mr-5" id="video-play" href="#">&nbsp;</a></div>
+          </div>
+        </div>
+        <div class="titan-caption">
+          <div class="caption-content">
+
+        <img src="assets/images/logo.png" alt="logo balangan" width="120">
+            <div class="font-alt mb-5 titan-title-size-4">Selamat Datang</div>
+            <div class=" titan-title-size-2 mb-30">Badan Kepegawaian Pendidikan dan Pelatihan daerah Kabupaten Balangan</div>
+            <a target="_blank" class="section-scroll btn btn-border-w btn-round btn-lg mb-10" href="http://web.bkppd-balangankab.info">
+             <span class="icon-newspaper" area-hidden="true"></span> Kunjungi Portal Web
+            </a>
+            <a target="_blank" class="section-scroll btn btn-border-w btn-round btn-lg mb-10" href="http://silka.bkppd-balangankab.info"><span class="icon-profile-male" area-hidden="true"></span> Kunjungi Simpeg</a>
+            <a target="_blank" class="section-scroll btn btn-border-w btn-round btn-lg mb-10" href="http://ekinerja.bkppd-balangankab.info"><span class="icon-wallet" area-hidden="true"></span> Kunjungi e-Kinerja</a>
+          </div>
+        </div>
+      </section>
+<div class="main">
+      <section class="module-extra-small bg-dark">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6 col-md-8 col-lg-6 col-lg-offset-2">
+                <div class="callout-text font-alt">
+                  <h4 style="margin-top: 0px; font-;">@timBKPPD</h4>
+                  <p style="margin-bottom: 0px;">&copy; 2019 Badan Kepegawaian Pendidikan dan Pelatihan Daerah</p>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-2">
+                <div class="callout-btn-box"><a class="btn btn-w btn-round" href="#">Semua Aplikasi BKPPD</a></div>
+              </div>
+            </div>
+          </div>
+        </section>
+          </div>
+      <!--
+      JavaScripts
+      =============================================
+      -->
+      <script src="assets/lib/jquery/dist/jquery.js"></script>
+      <script src="assets/lib/bootstrap/dist/js/bootstrap.min.js"></script>
+      <script src="assets/lib/wow/dist/wow.js"></script>
+      <script src="assets/lib/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.js"></script>
+      <script src="assets/lib/isotope/dist/isotope.pkgd.js"></script>
+      <script src="assets/lib/imagesloaded/imagesloaded.pkgd.js"></script>
+      <script src="assets/lib/flexslider/jquery.flexslider.js"></script>
+      <script src="assets/lib/owl.carousel/dist/owl.carousel.min.js"></script>
+      <script src="assets/lib/smoothscroll.js"></script>
+      <script src="assets/lib/magnific-popup/dist/jquery.magnific-popup.js"></script>
+      <script src="assets/lib/simple-text-rotator/jquery.simple-text-rotator.min.js"></script>
+      <script src="assets/js/plugins.js"></script>
+      <script src="assets/js/main.js"></script>
+    </body>
+  </html>
